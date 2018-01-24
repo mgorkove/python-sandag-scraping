7f91d95b82595f890fb139271d405c37c16f16ca
Masha Gorkovenko <mgmasha12@gmail.com>
2018-01-24 10:29:56 -0800

    Update create_directories.py

--- code/create_directories.py
+++ code/create_directories.py
@@ -2,7 +2,7 @@ import os, sys
 
 #create agendas, minutes, board actions, voting results directories, and in each of those directories, create text and pdf directories
 base = "/sandag scraping"
-existing_directories = ["borders", "executive", "public safety", "regional planning", "transportation"]
+existing_directories = ["borders", "executive", "public safety", "regional planning", "transportation"] # existing directories that I had in my filesystem
 d_to_add = ["agendas", "minutes", "board actions", "voting results"] # subdirectories to add to above directories
 subd_to_add = ["text", "pdf"] # subdirectories to add to above directories
 
